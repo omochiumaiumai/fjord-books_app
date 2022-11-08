42bd51f8808840acc824436dc710d1ff9827dbe3
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-11-08 12:36:34 +0900

    ログアウト機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!
+
+  def after_sign_in_path_for(resource)
+    books_url
+  end
+  def after_sign_out_path_for(resource)
+    '/users/sign_in'
+  end
+  def new
+    @user = current_user.id
+  end
+  
 end
