f5f70c2356af407fb685db26030f1dd7b0b955b3
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-11-14 12:44:38 +0900

    1ページに表示する件数を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    @users = Kaminari.paginate_array(@users).page(params[:page]).per(1)
+    @users = Kaminari.paginate_array(@users).page(params[:page]).per(10)
   end
 
   def show
