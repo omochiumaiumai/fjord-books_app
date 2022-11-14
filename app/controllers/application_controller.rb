4c38df6ae9262d3d4e513867c961184478ca5efc
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-11-14 12:48:35 +0900

    rubocopに指摘された問題の解決(記法の変更、マジックコメントの追加など)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    keys = [:postalcode, :address, :self_introduction]
+    keys = %i[postalcode address self_introduction]
     devise_parameter_sanitizer.permit(:account_update, keys: keys)
   end
 end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,6 +2,7 @@
 
 class Users::RegistrationsController < Devise::RegistrationsController
   protected
+
   def after_update_path_for(_resource)
     books_url
   end
