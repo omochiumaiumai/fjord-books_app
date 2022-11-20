5e4871a94efec9a3fcd3011a4db2afa3279f41a5
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-11-21 00:39:47 +0900

    self_introductionカラムのデータ型をtextに変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_20_151619) do
+ActiveRecord::Schema.define(version: 2022_11_20_153705) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 2022_11_20_151619) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "postal_code"
     t.string "address"
-    t.string "self_introduction"
+    t.text "self_introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
