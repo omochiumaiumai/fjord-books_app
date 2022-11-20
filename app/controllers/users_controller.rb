ba1c33eafb3b22221d2a825c1e7501ce08cf3f29
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-11-20 20:34:47 +0900

    paginateの処理を変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 class UsersController < ApplicationController
   def index
     @users = User.all
-    @users = Kaminari.paginate_array(@users).page(params[:page]).per(10)
+    @users = User.page(params[:page]).per(10)
   end
 
   def show
