998e4f708e30d4f711c0a97bd62659b41fc56fb4
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-11-29 23:41:32 +0900

    アソシエーションを設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
+
+  has_many :active_relationships, class_name: 'FollowRelation', foreign_key: :follower_id, dependent: :destroy, inverse_of: :follower
+  has_many :followings, through: :active_relationships, source: :following
+
+  has_many :passive_relationships, class_name: 'FollowRelation', foreign_key: :following_id, dependent: :destroy, inverse_of: :following
+  has_many :followers, through: :passive_relationships, source: :follower
 end
