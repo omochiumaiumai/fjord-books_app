6630ed1edb75eaf99f52b5d1d78f035a056c0c29
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-12-02 16:25:13 +0900

    follow_relationshipsコントローラーを作成/アクションを追加

---
+++ app/controllers/follow_relations_controller.rb
@@ -0,0 +1,19 @@
+class FollowRelationsController < ApplicationController
+  before_action :set_user
+
+  def create
+    current_user.follow(@user)
+    redirect_to user_path(@user)
+  end
+
+  def destroy
+    current_user.unfollow(@user)
+    redirect_to user_path(@user)
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:user_id])
+  end
+end
