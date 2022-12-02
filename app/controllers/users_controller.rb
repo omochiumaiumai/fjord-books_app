d987e37ab5321b64d73225b4ca061561e1567a60
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-12-02 21:29:50 +0900

    followings/followersメソッドを定義

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
+
   def index
     @users = User.with_attached_avatar.order(:id).page(params[:page])
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  def followings
+    @user = User.find(params[:id])
+    @followings = @user.followings.order(id: :desc).page(params[:page])
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @followers = @user.followers.order(id: :desc).page(params[:page])
   end
 end
