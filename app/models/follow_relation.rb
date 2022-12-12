d9a287c8e19095c0de31ca2088671035dcd272e5
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-12-12 16:18:23 +0900

    バリデーションを設定

--- app/models/follow_relation.rb
+++ app/models/follow_relation.rb
@@ -3,4 +3,5 @@
 class FollowRelation < ApplicationRecord
   belongs_to :follower, class_name: 'User'
   belongs_to :following, class_name: 'User'
+  validates :following_id, uniqueness: { scope: :follower_id }
 end
