6b9ba22d26f767cdabd00be75f9bec38a2db0e89
omochiumaiumai <shu.nakazato0419@gmail.com>
2022-12-20 22:43:01 +0900

    ルーティングを修正

--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resource :comments, only: %i[create destroy update], module: :books
+    resource :comments, only: [:create], module: :books
   end
 
   resources :reports do 
-    resources :comments, only: %i[create destroy update], module: :reports
+    resources :comments, only: [:create], module: :reports
   end 
 
   resources :users, only: %i[index show] do
